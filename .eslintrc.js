ea40c4ff1ab167add7a8e08f2c28c4161052b26d
dacho7 <davidtobarst@gmail.com>
2021-07-17 00:03:28 -0500

    fist commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,4 +11,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  // "[vue]": {
+  //   "editor.defaultFormatter": "esbenp.prettier-vscode"
+  // },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/registersales",
+    name: "RegisterSales",
+    component: () => import("../views/RegisterSales.vue"),
+  },
 ];
 
 const router = new VueRouter({
