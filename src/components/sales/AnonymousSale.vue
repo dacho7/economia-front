eb89aa453e54dfd589c93fbc2b12cb552ca5bce4
dacho7 <davidtobarst@gmail.com>
2021-08-20 17:06:28 -0500

    be added anonymous sale functionality

--- src/components/sales/AnonymousSale.vue
+++ src/components/sales/AnonymousSale.vue
@@ -13,12 +13,18 @@
           <v-row>
             <v-col>
               <v-text-field
-                v-model="product"
+                v-model="description"
                 label="descripción"
+                @keyup="findByDescription"
               ></v-text-field>
             </v-col>
             <v-col>
-              <v-select></v-select>
+              <v-select
+                v-if="allProducts.length >= 1"
+                v-model="product"
+                :items="showAllProducts"
+                label="Seleccione Producto"
+              ></v-select>
             </v-col>
           </v-row>
           <v-row>
@@ -26,17 +32,25 @@
               <h2>{{ productResult }}</h2>
             </v-col>
           </v-row>
-          <v-row v-if="product">
+          <v-row v-if="description && product">
             <v-col>
-              <v-text-field type="number" label="cantidad"></v-text-field>
+              <v-text-field
+                v-model="amount"
+                type="number"
+                label="cantidad"
+              ></v-text-field>
             </v-col>
             <v-col>
-              <v-text-field type="number" label="Total"></v-text-field>
+              <v-text-field
+                v-model="total"
+                type="number"
+                label="Total"
+              ></v-text-field>
             </v-col>
           </v-row>
-          <v-row v-if="!product">
+          <v-row v-if="!description">
             <v-col>
-              <v-text-field label="Total"></v-text-field>
+              <v-text-field v-model="total" label="Total"></v-text-field>
             </v-col>
           </v-row>
         </v-card-text>
@@ -56,12 +70,19 @@
 </template>
 
 <script>
+import { findProductByDescription } from "../../services/sales";
+
 export default {
   name: "AnonymousSale",
   props: ["dialog"],
   data() {
     return {
+      description: "",
       product: "",
+      idProduct: "",
+      amount: 1,
+      total: 0,
+      allProducts: [],
     };
   },
   computed: {
@@ -69,15 +90,44 @@ export default {
       if (this.product) {
         return this.product;
       }
-      return "Sin producto";
+      return "Sin Descripción";
+    },
+    showAllProducts() {
+      const products = [];
+      this.allProducts.forEach((element) => {
+        products.push(element.description);
+      });
+      return products;
     },
   },
   methods: {
     sendFather() {
       this.$emit("activate", {
-        name: true,
-        fal: false,
+        product: this.product,
+        amount: this.amount,
+        total: this.total,
+        idProduct: this.idProduct,
       });
+    },
+    findByDescription() {
+      if (this.description.length >= 3)
+        findProductByDescription(this.description).then((productsDB) => {
+          let prod = [];
+          console.log(productsDB);
+          if (productsDB.data.ok) {
+            productsDB.data.data.forEach((product) => {
+              prod.push(product);
+            });
+            this.allProducts = prod;
+          } else {
+            this.allProducts = [];
+            this.id = "";
+          }
+        });
+      if (this.description.trim().length < 3) {
+        this.allProducts = [];
+        this.product = "";
+      }
     },
   },
 };
