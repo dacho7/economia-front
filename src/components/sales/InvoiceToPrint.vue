cc9b3df7c2ab95ced996b6a7caa84eebef53cdce
dacho7 <davidtobarst@gmail.com>
2021-08-25 00:26:24 -0500

    completing print html component

--- src/components/sales/InvoiceToPrint.vue
+++ src/components/sales/InvoiceToPrint.vue
@@ -1,13 +1,41 @@
 <template>
   <div id="printJS-form">
     <div class="container">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Autoservicio La
+      Economia <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nit:
+      432432 <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3 Sector
+      Barrio Libertad
+      <br />
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Celular
+      3147581354
+      <div>
+        <table>
+          <tr>
+            <th>Producto</th>
+            <th>Precio</th>
+          </tr>
+          <tr>
+            <td>3x Papas Fritas</td>
+            <td>2000</td>
+          </tr>
+          <tr>
+            <td>coacola personal</td>
+            <td>1800000</td>
+          </tr>
+        </table>
+        Total: {{ total }}
+      </div>
+      <div v-if="client">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cliente:
+        {{ client }}
+      </div>
       <br />
       <br />
-      <h1>Autoservicio La Economia</h1>
-      <h3>Nit: 432432</h3>
-      <h3>3 Sector Barrio Libertad</h3>
+      <br />
+      .
     </div>
-    <v-container> v-simple </v-container>
   </div>
 </template>
 
@@ -15,6 +43,12 @@
 export default {
   name: "InvoiceToPrint",
   props: ["products"],
+  data() {
+    return {
+      client: "David Tobar",
+      total: 3800,
+    };
+  },
 };
 </script>
 
